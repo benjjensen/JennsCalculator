a691a623d9b5690b049751110510b430b9342446
Benjamin Jensen <benjjensen@Live.com>
2020-08-24 17:10:00 -0400

    Built web

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -13,7 +13,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "57b94951f901969adf4f27a4cd36761e",
 "/": "57b94951f901969adf4f27a4cd36761e",
-"main.dart.js": "28712ad86b27e83e2513c968966a09d4",
+"main.dart.js": "d98b152c82fddf2a1ef13d5398a17350",
 "manifest.json": "501cab88f567735bfd600f2bf57ea0bc"
 };
 
